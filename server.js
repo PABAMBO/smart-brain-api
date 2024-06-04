d031c2074b191fe18207a4cdc3217219fe5525bd
Phishego Ashleigh Bambo <maledibamboa@gmail.com>
2024-06-04 09:48:33 +0200

    fixed port

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT, () => { console.log(`App running on port ${process.env.PORT}`) })
+app.listen(5432, () => { console.log('App running on port 5432') })
